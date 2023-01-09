6f50e94576839b5ce03b71b254f8a0b635cbcfd8
anjanarodrigoz <85009096+anjanarodrigoz@users.noreply.github.com>
2023-01-09 23:55:02 +0530

    add get material page

--- insee/lib/main.dart
+++ insee/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
       theme: ThemeData(),
     );
